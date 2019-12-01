6ec8871df37119ca3e669290c756d1c09b137d30
giorgio <marly_96@hotmail.it>
2019-12-01 13:24:41 +0100

    add image validation and reading

--- client/imagic.py
+++ client/imagic.py
@@ -1,83 +1,34 @@
+from pathlib import Path
 import socket
 
 
-class Imagic:
-    def __init__(self, current_image, current_thumbs, image_validator, message_handler):
-        self.current_image = current_image
-        self.current_thumbs = current_thumbs
-        self.image_validator = image_validator
-        self.message_handler = message_handler
-
-    def validate_image(self, image_file):
-        valid = self.image_validator.validate(image_file)
-        if valid:
-            self.current_image = Image(image_file)
-        return valid
-
-    def select_category(self, category):
-        self.current_image.category = category
-
-    def upload_image(self):
-        self.message_handler.send_message(b'0', self.current_image)
-        received_message = self.message_handler.receive_message()
-        return received_message.payload
-
-    def find_thumbs(self, category):
-        self.message_handler.send_message(b'1', category)
-        received_message = self.message_handler.receive_message()
-        thumbs_dict = received_message.payload
-        self.current_thumbs = thumbs_dict
-        return self.current_thumbs
-
-    def show_image(self, thumb_file):
-        thumb_path = self.current_thumbs[thumb_file]
-        self.message_handler.send_message(b'2', thumb_path)
-        received_message = self.message_handler.receive_message()
-        image = received_message.payload
-        self.current_image = image
-        return self.current_image
-
-    def download_image(self):
-        return self.current_image.file
-
-
 class Image:
-    def __init__(self, image_file, category=None, thumb_file=None):
+    def __init__(self, image_file):
         self.image_file = image_file
-        self.category = category
-        self.thumb_file = thumb_file
+        self.category = None
+        self.thumb_file = None
 
 
 class ImageValidator:
-    def validate(self, image):
-        pass
+    def __init__(self):
+        self.image_extensions = self.load_image_extensions()
 
+    @staticmethod
+    def load_image_extensions():
+        return '.jpg', '.png', '.gif', '.tif'
 
-class MessageHandler:
-    def __init__(self, message, load_balancer_connector):
-        self.message = message
-        self.load_balancer_connector = load_balancer_connector
+    def validate(self, image_file_path):
+        file_extension = Path(image_file_path).suffix
+        if file_extension in self.image_extensions:
+            return self.get_image(image_file_path)
+        return None
 
-    def send_message(self, message_type, payload):
-        header = Header(message_type)
-        message = Message(header, payload)
-        self.load_balancer_connector.send(message)
-
-    def receive_message(self):
-        return self.load_balancer_connector.receive()
-
-
-class Message:
-    def __init__(self, header, payload):
-        self.header = header
-        self.payload = payload
-
-
-class Header:
-    def __init__(self, message_type, source=None, destination=None):
-        self.message_type = message_type
-        self.message_type = source
-        self.message_type = destination
+    @staticmethod
+    def get_image(image_file_path):
+        with open(image_file_path, "rb") as f:
+            image_file = f.read()
+            f.close()
+            return image_file
 
 
 class Host:
@@ -86,19 +37,30 @@ class Host:
         self.port = port
 
 
+class Header:
+    def __init__(self, message_type=None):
+        self.message_type = message_type
+        self.source = None
+        self.destination = None
+        self.payload_length = None
+
+
+class Message:
+    def __init__(self):
+        self.header = Header()
+        self.payload = None
+
+
 class LoadBalancerConnector:
-    def __init__(self, source, destination, message_length=1024, sock=None):
-        self.source = source
-        self.destination = destination
-        self.message_length = message_length
-
-        if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-        self.sock.connect((destination.address, destination.port))
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.destination = Host("localhost", 5000)
+        self.client_socket.connect((self.destination.address, self.destination.port))
+        self.source = self.client_socket.getsockname()
+        self.message_length = 1024
         print("Socket connection opened!")
+        print("Source address:", self.source)
+        print("Destination address:", self.destination)
 
     def send(self, message):
         message.header.source = self.source
@@ -106,16 +68,70 @@ class LoadBalancerConnector:
 
         total_sent = 0
         while total_sent < self.message_length:
-            sent = self.sock.send(message[total_sent:])
+            sent = self.client_socket.send(message[total_sent:])
             if sent == 0:
                 raise RuntimeError("Socket connection closed!")
             total_sent = total_sent + sent
 
     def receive(self):
-        message = ''
+        message = b''
         while len(message) < self.message_length:
-            chunk = self.sock.recv(self.message_length - len(message))
-            if chunk == '':
+            chunk = self.client_socket.recv(self.message_length - len(message))
+            if chunk == b'':
                 raise RuntimeError("Socket connection closed!")
             message = message + chunk
         return message
+
+
+class MessageHandler:
+    def __init__(self):
+        self.current_message = Message()
+        self.load_balancer_connector = LoadBalancerConnector()
+
+    def send_message(self, message_type, payload):
+        self.current_message.header.message_type = message_type
+        self.current_message.payload = payload
+        self.load_balancer_connector.send(self.current_message)
+
+    def receive_message(self):
+        return self.load_balancer_connector.receive()
+
+
+class Imagic:
+    def __init__(self):
+        self.current_image = None
+        self.current_thumbs = None
+        self.image_validator = ImageValidator()
+        self.message_handler = MessageHandler()
+
+    def validate_image(self, image_file_path):
+        image_file = self.image_validator.validate(image_file_path)
+        if image_file:
+            self.current_image = Image(image_file)
+        return image_file
+
+    def select_category(self, category):
+        self.current_image.category = category
+
+    def upload_image(self):
+        self.message_handler.send_message(0, self.current_image)
+        received_message = self.message_handler.receive_message()
+        return received_message.payload
+
+    def find_thumbs(self, category):
+        self.message_handler.send_message(1, category)
+        received_message = self.message_handler.receive_message()
+        thumbs_dict = received_message.payload
+        self.current_thumbs = thumbs_dict
+        return self.current_thumbs
+
+    def show_image(self, thumb_file):
+        thumb_file_path = self.current_thumbs[thumb_file]
+        self.message_handler.send_message(2, thumb_file_path)
+        received_message = self.message_handler.receive_message()
+        image = received_message.payload
+        self.current_image = image
+        return self.current_image
+
+    def download_image(self):
+        return self.current_image.file
