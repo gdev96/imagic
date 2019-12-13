65d83dfde0efcce0cf47133acaf5e542029866fd
Marco Mannino <ilmanno96@gmail.com>
2019-12-13 10:24:31 +0100

    fixed lb bugs and created created server folder

---
+++ server/Constants.h
@@ -0,0 +1,39 @@
+#ifndef IMAGIC_BACKEND_CONSTANTS_H
+#define IMAGIC_BACKEND_CONSTANTS_H
+
+//INCLUDES
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <pthread.h>
+#include "Message.h"
+
+//NAMESPACES
+using  namespace std;
+
+//CONNECTOR
+#define CONNECTOR_CLIENT_ADDRESS "127.0.0.1"
+#define CONNECTOR_CLIENT_PORT 5000
+#define CONNECTOR_SERVER_ADDRESS "127.0.0.1"
+#define CONNECTOR_SERVER_FIRST_PORT 6000
+#define CONNECTOR_SERVER_OFFSET_PORT 10
+#define BUFFER_SIZE 10000000 //bytes
+#define QUEUE_LENGTH_CONNECTIONS 5
+
+//HEADER
+#define HEADER_LENGTH 9
+#define MESSAGE_TYPE 1
+#define SOURCE_ID 4
+#define PAYLOAD_LENGTH 4
+
+//LOAD BALANCER
+#define N_SERVER 3
+#define MAX_SERVER 10
+
+#endif //IMAGIC_BACKEND_CONSTANTS_H
---
+++ server/Message.h
@@ -0,0 +1,36 @@
+#ifndef IMAGIC_BACKEND_MESSAGE_H
+#define IMAGIC_BACKEND_MESSAGE_H
+
+#include <cstdint>
+#include <ostream>
+
+class Header {
+    private:
+        unsigned char message_type;
+        uint32_t source_id;
+        uint32_t payload_length;
+    public:
+        Header();
+        unsigned char getMessageType() const;
+        uint32_t getSourceId() const;
+        uint32_t getPayloadLength() const;
+        void setMessageType(unsigned char messageType);
+        void setSourceId(uint32_t sourceId);
+        void setPayloadLength(uint32_t payloadLength);
+        void serialize(unsigned char *buffer);
+        void deserialize(unsigned char *buffer);
+        friend std::ostream &operator<<(std::ostream &os, const Header &header);
+};
+
+class Message {
+    private:
+        Header *header;
+        unsigned char *payload;
+    public:
+        Message(Header *header, unsigned char *payload);
+        ~Message();
+        Header *getHeader() const;
+        unsigned char *getPayload() const;
+};
+
+#endif //IMAGIC_BACKEND_MESSAGE_H
