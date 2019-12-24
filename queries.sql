146d2e063bc9cb3da6f09b17cf50686a423519e6
giorgio <uni371032@studium.unict.it>
2019-12-24 19:46:38 +0100

    delete queries.sql file

--- queries.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS imagic;\nCREATE USER IF NOT EXISTS
-  'imagicuser'@'%' identified by 'ImgApp2020!';\nGRANT ALL ON imagic.* TO 'imagicuser'@'%';\nCREATE TABLE IF NOT EXISTS imagic.image (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  image_file_path VARCHAR(45) NULL,
-  thumb_file_path VARCHAR(45) NULL,
-  category VARCHAR(45) NULL
-) ENGINE = InnoDB;
