8a6ec9cce3b1ba3ad475f5db989c68713c869dd5
giorgio <uni371032@studium.unict.it>
2020-03-08 15:23:22 +0100

    Add support for GIFs

--- client/imagic.py
+++ client/imagic.py
@@ -14,6 +14,8 @@ class Imagic:
         image_file = self.image_validator.validate(image_file_path)
         if image_file:
             self.current_image = Image(image_file)
+            return True
+        return False
 
     def select_category(self, category):
         self.current_image.category = category
@@ -26,11 +28,11 @@ class Imagic:
         received_payload = self.message_handler.send_message(MessageType.FIND_THUMBS, category)[1]
         self.current_thumbs = received_payload
 
-    def show_image(self, thumb_file_name):
+    def download_image(self, thumb_file_name):
         received_payload = self.message_handler.send_message(MessageType.DOWNLOAD_IMAGE, thumb_file_name)[1]
         self.current_image = Image(received_payload)
 
-    def download_image(self, image_path):
+    def save_image(self, image_path):
         with open(image_path, "wb") as f:
             f.write(self.current_image.image_file)
             f.close()
